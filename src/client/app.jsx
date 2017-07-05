239599064f0d01030bb65a851c9d1b6513d96f55
Zachary Smith <william.smith1@wipro.com>
2017-07-05 17:03:47 -0400

    Poll Travis every ten seconds for build status
    Display the time last updated

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import moment from 'moment'
 import fetchRepoStatus from './services/fetchRepoStatus'
+import TimeStamp from './components/TimeStamp'
 
 class App extends React.Component {
   constructor() {
@@ -7,38 +9,47 @@ class App extends React.Component {
     this.state = {
       repo: 'buildit/bookit-web',
       status: {},
+      lastUpdated: {},
     }
   }
 
   componentDidMount() {
-    fetchRepoStatus(this.state.repo)
-      .then((status) => {
-        this.setState({ status })
-      })
+    const POLLING_INTERVAL = 10000 // milliseconds
+    setInterval(() => {
+      fetchRepoStatus(this.state.repo)
+        .then((status) => {
+          const lastUpdated = moment()
+          this.setState({ lastUpdated, status })
+        })
+    }, POLLING_INTERVAL)
   }
 
   render() {
-    const status = this.state.status
+    const { status, lastUpdated } = this.state
     return (
       <div>
-        <div style={{
-          display: 'flex', alignItems: 'baseline',
-        }}
-        >
-          <span style={{
-            margin: '0 0.5rem',
-            width: '0.7rem',
-            height: '0.7rem',
-            display: 'inline-block',
-            background: status.result ? '#37de0d' : '#ee1497',
-            borderRadius: '50%',
+        <div className="statusList">
+          <div style={{
+            display: 'flex',
+            alignItems: 'baseline',
           }}
-          />
-          <span style={{
-            margin: '0',
-          }}
-          >Bookit web</span>
+          >
+            <span style={{
+              margin: '0 0.5rem',
+              width: '0.7rem',
+              height: '0.7rem',
+              display: 'inline-block',
+              background: status.result ? '#37de0d' : '#ee1497',
+              borderRadius: '50%',
+            }}
+            />
+            <span style={{
+              margin: '0',
+            }}
+            >Bookit web</span>
+          </div>
         </div>
+        <TimeStamp lastUpdated={lastUpdated} />
       </div>
     )
   }
