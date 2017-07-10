20d8f42b0cf69483b7d49db48abddc5d816e91cc
Z <BillyZac@users.noreply.github.com>
2017-07-10 16:07:06 -0400

    Emojify (#36)
    * Use emoji instead of colors
    
    * Use emoji instead of colored dots to indicate status
    
    Colored dots (especially red and green ones) are hard to distinguish for
    people with color blindness. This commit replaces the colored dots with
    emoji 😎  💀  😎  💀  😎  💀  😎  💀
    
    I also added aria labels, for people who can't see emojis either.

--- src/client/components/ProjectStatus.jsx
+++ src/client/components/ProjectStatus.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react'
+import StatusIndicator from './StatusIndicator'
 
 type Props = {
   name: string,
@@ -9,33 +10,10 @@ type Props = {
 }
 
 const ProjectStatus = ({ name, buildResult, pingResult }: Props) => (
-  <div style={{
-    display: 'flex',
-    alignItems: 'baseline',
-  }}
-  >
-    <span style={{
-      margin: '0 0.2rem 0 0',
-      width: '0.7rem',
-      height: '0.7rem',
-      display: 'inline-block',
-      background: buildResult ? '#37de0d' : '#ee1497',
-      borderRadius: '50%',
-    }}
-    />
-    <span style={{
-      margin: '0 0.2rem 0 0',
-      width: '0.7rem',
-      height: '0.7rem',
-      display: 'inline-block',
-      background: pingResult ? '#37de0d' : '#ee1497',
-      borderRadius: '50%',
-    }}
-    />
-    <span style={{
-      margin: '0',
-    }}
-    >{ name }</span>
+  <div>
+    <StatusIndicator result={buildResult} />
+    <StatusIndicator result={pingResult} />
+    <span style={{ margin: '0' }}>{ name }</span>
   </div>
   )
 
